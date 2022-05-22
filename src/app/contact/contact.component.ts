cb0eeb6eeec72fca989cc5201fddd30a2bdd1aa6
Gunars Lazdans <gunars.lazdans@gmail.com>
2022-05-22 13:48:44 +0200

    changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AppserviceService } from '../appservice.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  constructor(private appService: AppserviceService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addClass($event: any) {
+    this.appService.onIntersection($event);
   }
-
 }
