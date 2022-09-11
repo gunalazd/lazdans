db6ee7c4474ba0b4e42d37dd4965cc3fed6cca1c
Gunars Lazdans <gunars.lazdans@gmail.com>
2022-09-11 15:35:51 +0200

    profile animation delay on each circle animation

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
   constructor() {}
+  element = document.getElementsByClassName('spinner');
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.showAnimationObj();
+  }
+
+  showAnimationObj() {
+    for (var i = 0; i < this.element.length; i++) {
+      this.doSetTimeout(i);
+    }
+  }
+
+  doSetTimeout(i: number) {
+    setTimeout(() => {
+      this.element[i].classList.add('show-me');
+    }, i * 750);
+  }
 }
